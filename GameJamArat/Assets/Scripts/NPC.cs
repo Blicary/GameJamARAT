911d049d20fe37665eb05c3b75031d35eb6105e7
Robert Wild <wildr2@rpi.edu>
2014-09-20 17:25:21 -0400

    Update Scripts

--- GameJamArat/Assets/Scripts/NPC.cs
+++ GameJamArat/Assets/Scripts/NPC.cs
@@ -1,27 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using Time;
 
 public class NPC : MonoBehaviour 
 {
-    public float move_speed;
-    private Vector2 target_pos;
-
-    
     public string ID_name;
     private List<string> tags = new List<string>();
 
-    //public string aspiration;
+    public float move_speed;
+    private Vector2 target_pos;
+
+    public float listen_percent;
+
+
+
+    // PRIVATE MODIFIERS
+    private void UpdateListenArea()
+    {
+        Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, 10, 9);
+        for (int i = 0; i < col.Length; ++i)
+        {
+            WorldComponent wc = col[i].gameObject.GetComponent<WorldComponent>();
+            wc.TurnOn();
+        }
+    }
 
 
     // PUBLIC MODIFIERS
+    public void Update()
+    {
+        UpdateListenArea();
+    }
+
     public void Move(Vector2 dest)
     {
-        Vector2.Distance(dest, transform.position);
-
+        //...
     }
-    
+
+    public void IncreaseListen(float amount)
+    {
+        listen_percent += amount;
+        UpdateListenArea();
+    }
+    public void DecreaseListen(float amount)
+    {
+        listen_percent -= amount;
+        UpdateListenArea();
+    }
+
     // PUBLIC ACCESSORS
-    public bool HasTag (string tag) { return tags.Contains(tag); }
+    public bool HasTag(string tag) { return tags.Contains(tag); }
+    public float GetListen() { return listen_percent; }
 }
--- GameJamArat/Assets/Scripts/WorldComponent.cs
+++ GameJamArat/Assets/Scripts/WorldComponent.cs
@@ -7,14 +7,38 @@ public class WorldComponent : MonoBehaviour
 {
     public List<string> tags = new List<string>();
 
+    private float live_time_max = 2;
+    private float live_time;
+
+    
 
     // PUBLIC MODIFIERS
+    public void Start()
+    {
+        live_time = live_time_max;
+    }
+
+    public void Update()
+    {
+        // time before fizzles out
+        if (live_time >= 0)
+        {
+            live_time -= Time.deltaTime;
+        }
+        else
+        {
+            TurnOff();
+        }
+    }
+
     public void TurnOn()
     {
+        live_time = live_time_max;
         gameObject.SetActive(true);
     }
     public void TurnOff()
     {
+        live_time = 0;
         gameObject.SetActive(false);
     }
 
