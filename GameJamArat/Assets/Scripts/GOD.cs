2b406111858dc665e59de43b4797915fdcfb5ec4
duneta <duneta@rpi.edu>
2014-09-20 17:22:09 -0400

    God class update

--- GameJamArat/Assets/Scripts/GOD.cs
+++ GameJamArat/Assets/Scripts/GOD.cs
@@ -3,18 +3,37 @@ using System.Collections;
 
 public class GOD : MonoBehaviour {
 
-    public max_radius = 1.0;            // The total listen radius possible.
-    private float listen_avail;         // The net amount of listen available.
+    public float max_radius = 1.0;            // The total listen radius possible.
+    private float listen_avail = max_radius;        // The net amount of listen available.
 
     private NPC active_NPC;             // The NPC that is currently selected to recieve listen.
-    private float command_scroll;       // The amount of listen to be imparted to the selected NPC.    
+    private float command_scroll = 0.0;      // The amount of listen to be imparted to the selected NPC.    
 	
 	// Update is called once per frame
-	void Update () {
-	    
+	public void Update () {
+	    if (active_NPC != null)
+        {
+            command_scroll += Input.GetAxis("Mouse ScrollWheel"); // How much listen does the player want to spend?
+            if (command_scroll > listen_avail) // Don't let the player spend moar listen than they have.
+            {
+                command scroll = listen_avail;
+            }
+        }
 	}
+
+    
 
     // PUBLIC MODIFIERS
     public void Set_Active(NPC npc) {active_NPC=npc;}      // Set the NPC currently receiving commands.
     public void Clear_Active() {active_NPC=null;}          // No NPC is currently recieving commands.
+    
+    // PUBLIC HELPERS
+    
+    public void OnMouseDown()                                       // The player confirm listen expendature.
+    {
+        if ( (command_scroll > 0) && (active_NPC != null) )
+        {
+            //if (Get_Listen(active_NPC) > )
+        }
+    }
 }
