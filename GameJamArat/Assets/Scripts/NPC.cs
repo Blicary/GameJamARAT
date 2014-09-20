d1e6f8eaa4a0055f6d2a11311d6bf52569b3351f
Robert Wild <wildr2@rpi.edu>
2014-09-20 16:18:38 -0400

    Update scripts

--- GameJamArat/Assets/Scripts/NPC.cs
+++ GameJamArat/Assets/Scripts/NPC.cs
@@ -2,32 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using Time;
-using mathf;
 
-public class NPC : MonoBehaviour {
-
-    private move_speed;
-    private move_dest;
+public class NPC : MonoBehaviour 
+{
+    public float move_speed;
+    private Vector2 target_pos;
 
     public string ID_name;
-    private List<string> tags = new List<string>;
+    private List<string> tags = new List<string>();
 
     //public string aspiration;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    public bool HasTag (string itag) {return tags.Contains(itag);}
-
-    public void Move (Vector2 idest) 
+    // PUBLIC MODIFIERS
+    public void Move(Vector2 dest)
     {
-        Vector2.Distance(idest,transform.position);
+        Vector2.Distance(dest, transform.position);
     }
+    
+    // PUBLIC ACCESSORS
+    public bool HasTag (string tag) { return tags.Contains(tag); }
 }
