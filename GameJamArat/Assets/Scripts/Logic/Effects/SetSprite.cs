090eedeedb71d7edbc70849254befcedec723cdb
duneta <duneta@rpi.edu>
2014-09-21 12:06:54 -0400

    Level2 stuff

---
+++ GameJamArat/Assets/Scripts/Logic/Effects/SetSprite.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetSprite : Effect {
+
+    public Transform Rend_obj;
+
+    public Sprite spr1;
+
+    public override void Do()
+    {
+        SpriteRenderer spr_rend = Rend_obj.GetComponent<SpriteRenderer>();
+
+        spr_rend.sprite = spr1;
+
+    }
+}
