f9a0d02effe29b0308e033c47093b0f1cacc030c
Robert Wild <wildr2@rpi.edu>
2014-09-21 10:49:20 -0400

    lolli fix

--- GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
+++ GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
@@ -9,10 +9,8 @@ public class OnClickAt : Condition
 
     public override bool Met()
     {
-
-        if (target.collider2D.bounds.Contains(Camera.main.ScreenToWorldPoint(Input.mousePosition)) && Input.GetMouseButtonDown(0))
+        if (target.collider2D.bounds.Contains((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition)) && Input.GetMouseButtonDown(0))
         {
-            Debug.Log("click at");
             return true;
         }
         else
