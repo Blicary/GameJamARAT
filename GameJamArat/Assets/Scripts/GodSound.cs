1538a57076c3a22eb262d3aabcc178e40b699866
Robert Wild <wildr2@rpi.edu>
2014-09-21 06:46:22 -0400

    sound

---
+++ GameJamArat/Assets/Scripts/GodSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodSound : MonoBehaviour 
+{
+    public GOD god;
+
+    public AudioSource music_see_more; // when more dark
+    public AudioSource music_see_less;  // when less dark
+
+
+    public void Start()
+    {
+        music_see_more.Play();
+        music_see_less.Play();
+    }
+    public void Update()
+    {
+        
+        music_see_more.volume = 1.0f - god.listen_avail;
+        music_see_less.volume = 1;
+
+        Debug.Log(god.listen_avail);
+    }
+	
+}
