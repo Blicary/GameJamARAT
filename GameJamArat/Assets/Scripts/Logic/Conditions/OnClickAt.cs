606ec98b03873f11cd068988401721afc01264e6
duneta <duneta@rpi.edu>
2014-09-21 09:19:44 -0400

    click

--- GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
+++ GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
@@ -6,17 +6,21 @@ public class OnClickAt : Condition
 
     public Transform location;
     public float radius;
+    public Camera cam;
 
 	public override bool Met()
 	{
-        Debug.Log("EnterIf");
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("mouse");
-            Vector3 mouseget = Input.mousePosition;
+            Vector3 mouseget = cam.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mouseloc = new Vector2(mouseget.x, mouseget.y);
 
             Vector2 objloc = new Vector2(location.position.x,location.position.y);
+
+            Debug.Log("Click");
+            Debug.Log(Vector2.Distance(mouseloc, objloc));
+            Debug.Log(mouseloc);
+            Debug.Log(objloc);
             return Vector2.Distance(mouseloc, objloc) < radius;
         }
         else
