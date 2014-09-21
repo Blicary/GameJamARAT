e58aa8683a7d2ac891749a89712ed32934929bd0
Robert Wild <wildr2@rpi.edu>
2014-09-21 10:40:29 -0400

    on click at

--- GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
+++ GameJamArat/Assets/Scripts/Logic/Conditions/OnClickAt.cs
@@ -4,28 +4,20 @@ using System.Collections;
 public class OnClickAt : Condition
 {
 
-    public Transform location;
+    public Transform target;
     public float radius;
-    public Camera cam;
 
-	public override bool Met()
-	{
-        if (Input.GetMouseButtonDown(0))
+    public override bool Met()
+    {
+
+        if (target.collider.bounds.Contains(Input.mousePosition) && Input.GetMouseButtonDown(0))
         {
-            Vector3 mouseget = cam.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mouseloc = new Vector2(mouseget.x, mouseget.y);
-
-            Vector2 objloc = new Vector2(location.position.x,location.position.y);
-
-            Debug.Log("Click");
-            Debug.Log(Vector2.Distance(mouseloc, objloc));
-            Debug.Log(mouseloc);
-            Debug.Log(objloc);
-            return Vector2.Distance(mouseloc, objloc) < radius;
+            Debug.Log("click at");
+            return true;
         }
         else
         {
             return false;
         }
-	}
+    }
 }
