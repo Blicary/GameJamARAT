8a8ea631e9cd79db027216c1b79fb1d85475444e
duneta <duneta@rpi.edu>
2014-09-21 06:09:58 -0400

    Level One Complete

---
+++ GameJamArat/Assets/Scripts/Logic/Conditions/Distance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Distance : Condition
+{
+    public Transform object1;
+    public Transform object2;
+    public float distance;
+	// Use this for initialization
+    public override bool Met()
+    { 
+        return (object1.position - object2.position).magnitude < distance;
+    }
+}
